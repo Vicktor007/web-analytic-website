6492c5e320082c818f2933893fa580aa04424325
Vicktor007 <vicktord007@gmail.com>
2025-04-22 07:49:25 +0100

    backend started

--- src/app/(auth)/welcome/page.tsx
+++ src/app/(auth)/welcome/page.tsx
@@ -3,6 +3,10 @@ import { LoadingSpinner } from "@/components/loading-spinner"
 import { LucideProps } from "lucide-react"
 
 const Page = () => {
+
+    const {} = useQuery({
+        
+    })
     return (
         <div className="flex w-full flex-1 items-center justify-center px-4">
             <BackgroundPattern className="absolute inset-0 left-1/2 z-0 -translate-x-1/2 opacity-75 "/>
