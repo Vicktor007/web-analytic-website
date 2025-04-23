2cc68dd9204d4b46c5d9d6b7d8ac94665bd74f57
Vicktor007 <vicktord007@gmail.com>
2025-04-23 07:00:23 +0100

    save

--- src/server/procedures.ts
+++ src/server/procedures.ts
@@ -1,4 +1,7 @@
+import { db } from "@/db"
 import { j } from "./__internals/j"
+import { currentUser } from "@clerk/nextjs/server"
+import { HTTPException } from "hono/http-exception"
 
 // /**
 //  * Middleware for providing a built-in cache with your Prisma database.
@@ -33,5 +36,40 @@ import { j } from "./__internals/j"
 //  *
 //  * This is the base piece you use to build new queries and mutations on your API.
 //  */
+
+const authMiddleware = j.middleware(async ({c, next})=> {
+    const authHeader = c.req.header("Authorization")
+
+    if(authHeader) {
+        const apiKey = authHeader.split("")[1] // bearer  <api key>
+
+        const user = await db.user.findUnique({
+            where:{apiKey},
+        })
+
+        if(user) return next({user})
+    }
+
+    const auth = await currentUser();
+
+
+    if(!auth){
+        throw new HTTPException(401, {message: "Unauthorized"})
+    }
+
+    const user = await db.user.findUnique({
+        where: {externalId: auth.id},
+    })
+
+    if(!user){
+        throw new HTTPException(401, {message: "Unauthorized"})
+    }
+
+    return next({
+        user
+    })
+})
+
 export const baseProcedure = j.procedure
 export const publicProcedure = baseProcedure
+export const privateProcedure = publicProcedure.use(authMiddleware)
