1dc1c8e003c22426a3c05bdbe14aba9a65b90ecd
Vicktor007 <vicktord007@gmail.com>
2025-05-10 05:31:42 +0100

    route issue solved

--- src/app/api/trackings/route.ts
+++ src/app/api/trackings/route.ts
@@ -2,18 +2,28 @@ import { db } from "@/db";
 import { NextResponse } from "next/server";
 
 
-export const corsHeaders = {
+
+
+
+export async function OPTIONS(){
+
+    const corsHeaders = {
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
     "Access-Control-Allow-Headers": "Content-Type, Authorization",
 }
 
-
-export async function OPTIONS(){
     return NextResponse.json({}, {headers: corsHeaders});
   }
 
   export async function POST(request: { json: () => any; }){
+
+    const corsHeaders = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+    "Access-Control-Allow-Headers": "Content-Type, Authorization",
+}
+
     const response = await request.json();
     const {domain, url, event, source} = response;
 
