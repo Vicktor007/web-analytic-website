2120c78b3f5cd54164c48e203b55f321aa772a57
Vicktor007 <vicktord007@gmail.com>
2025-08-14 08:54:29 +0100

    save

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import { Inter } from "next/font/google"
 import { Providers } from "../components/providers"
 import { EB_Garamond } from "next/font/google"
 import { cn } from "@/utils"
-
 import "./globals.css"
 import { ClerkProvider } from "@clerk/nextjs"
 import Script from "next/script"
