f81338d6ae7c14132930146885bb6f3556c4f590
Vicktor007 <vicktord007@gmail.com>
2025-08-14 08:47:20 +0100

    save

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import { cn } from "@/utils"
 
 import "./globals.css"
 import { ClerkProvider } from "@clerk/nextjs"
+import Script from "next/script"
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-sans" })
 const eb_garamond = EB_Garamond({
@@ -28,6 +29,11 @@ export default function RootLayout({
     <ClerkProvider>
     <html lang="en" className={cn(inter.variable, eb_garamond.variable)}>
       <body className=" min-h-[calc(100vh-1px)] flex flex-col  font-sans bg-brand-50 text-brand-950 antialiased">
+      <Script
+            defer
+            data-domain="pingpanda-mbc8.onrender.com"
+            src="https://pingpanda-mbc8.onrender.com/tracking-script.js"
+          />
         <main className="relative flex-1 flex flex-col">
         <Providers>{children}</Providers>
         </main>
